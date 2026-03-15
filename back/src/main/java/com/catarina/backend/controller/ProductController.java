0ef524f735fd33f8ae6677014de31b874a7a8f5f
Catarina Fernandes <catarina@Catarinas-MacBook-Air.local>
2026-03-15 00:07:54 +0000

    feat:implement GET/products endpoint using ProductService

--- back/src/main/java/com/catarina/backend/controller/ProductController.java
+++ back/src/main/java/com/catarina/backend/controller/ProductController.java
@@ -2,15 +2,29 @@ package com.catarina.backend.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import com.catarina.backend.service.ProductService;
+import com.catarina.backend.model.Product;
 
+
+import java.util.List;
 
 
 @RestController
 public class ProductController {
 
-    @GetMapping("/products")
-    public String test() {
-        return "endpoint of products is working";
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
     }
+
+    @GetMapping("/products")
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }    
+
+    //public String test() {
+        //return "endpoint of products is working";
+    
     
 }
